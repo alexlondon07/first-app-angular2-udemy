c2b72ce2f2281601e53e397b319610b37311bc8c
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-11-30 18:07:46 -0500

    creating a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Angularjs Version 2 Udemy!<h1>
+    <app-other></app-other>
+  `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angularjs Version 2 Udemy!';
