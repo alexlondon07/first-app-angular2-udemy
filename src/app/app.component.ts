70fd823861c75690371be7f846eea9f60e7ac4ac
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-12-02 14:19:15 -0500

    Inserting Content with ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,22 @@ import { Component } from '@angular/core';
   template: `
     <h1>Angularjs Version 2 Udemy!<h1>
     <app-other></app-other>
+    <app-another>
+          <div>
+            <p>Hello</p>
+            <p>World</p>
+          </div>
+    </app-another>
+    <app-another>Something</app-another>
   `,
+    styles:[`
+      h1 {
+          color: blue;
+      }
+    `]
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angularjs Version 2 Udemy!';
+  title = '';
 }
--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-other',
   templateUrl: './other.component.html',
-  styleUrls: ['./other.component.css']
 })
 export class OtherComponent implements OnInit {
 
